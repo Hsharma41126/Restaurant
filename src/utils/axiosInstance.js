01709f69217a007af50afa10c227a358506a776c
ap8114 <amanpatidar086@gmail.com>
2025-08-19 17:30:20 +0530

    pull by aop

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -11,7 +11,7 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("authToken");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -24,7 +24,7 @@ axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem("token");
+      localStorage.removeItem("authToken");
       window.location.href = "/login";
     }
     return Promise.reject(error);
